608a67a3216cb89fca8f2b51355fa8a8c1270bac
João Lucas <joao162002009@gmail.com>
2026-05-15 20:20:06 -0300

    fix: update CORS and add migration pre-deploy

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(helmet());
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://whitexcorporation.com.br'],
+  origin: ['https://whitexcorporation.com.br', 'http://localhost:5173'],
   credentials: true,
 }));
 app.use(express.json());
